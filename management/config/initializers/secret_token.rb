670062c8bd0b35c75b6370c62d25b26d68d1b995
Kyle Lady <kylelady@umich.edu>
2012-09-01 16:10:06 +0000

    Management: initial Rails

---
+++ management/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Management::Application.config.secret_token = '8ab679d7f1ace4bd14e51ed93ffd810a3960c15100128f33f79e82a41aace4e89ff0e4d960d24a55405b7b21dadef652f94e83b4647682e1467f5865ecd1778c'
