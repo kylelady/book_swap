e5133074e01b610900e726e4482bda5be159efeb
mikehand <mikehand@umich.edu>
2012-09-02 20:58:36 -0400

    edited the real files this time, also changed some nav titles

--- public/webcontent/includes/header.js
+++ public/webcontent/includes/header.js
@@ -1,5 +1,5 @@
 document.write("<header>");
-document.write("<img src=\"TBPlogoHiRes.gif\" alt=\"TBP Logo\" width=250/>");
+document.write("<img src=\"TBPlogoHiRes_small.gif\" alt=\"TBP Logo\"/>");
 document.write("<hgroup>");
 document.write("<h1>Engineering Bookswap</h1>");
 document.write("<h2>Brought to you by Tau Beta Pi</h2>");
@@ -10,7 +10,7 @@ document.write("<nav>");
 document.write("<ul>");
 document.write("<li><a id=\"AboutTBP\" href=\"AboutTBPMIG.html\">About Tau Beta Pi: MI-&Gamma;</a></li>");
 document.write("<li><a id=\"Inventory\" href=\"BookswapInventory.html\">Current Inventory</a></li>");
-document.write("<li><a id=\"AboutBookswap\" href=\"AboutBookswap.html\">About Bookswap</a></li>");
+document.write("<li><a id=\"FAQ\" href=\"AboutBookswap.html\">FAQ</a></li>");
 document.write("<li><a id=\"FindUs\" href=\"BookswapLocation.html\">Find Us</a></li>");
 document.write("</ul>");
 document.write("</nav>");
