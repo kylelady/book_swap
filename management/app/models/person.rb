e291c84d6e0fda6989de842f44f0233b2f99a23e
Kyle Lady <kylelady@umich.edu>
2012-09-03 02:42:49 +0000

    UI for barcode

--- management/app/models/person.rb
+++ management/app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
-  attr_accessible :address1, :address2, :city, :state, :umid, :uniqname, :zip
+  attr_accessible :address1, :address2, :city, :state, :umid, :uniqname, :zip, :barcode
 	has_many :selling_books, :class_name => "Book", :through => :seller
 	has_many :bought_books, :class_name => "Book", :through => :buyer
 end
