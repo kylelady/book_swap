1b2b90386f1286da6fc3321853e05f46a7ace5da
Kyle Lady <kylelady@umich.edu>
2012-09-03 02:32:03 +0000

    added analytics

--- public/LoadInventory.py
+++ public/LoadInventory.py
@@ -1,4 +1,4 @@
-from datetime import datetime 
+from datetime import datetime
 import csv
 import os
 inventory_path="inventory.csv"
@@ -27,6 +27,19 @@ gen_html.write(r'''<!doctype html>
 <script src="sorttable.js"></script>
 <script type="text/javascript">
 var pagename="Inventory";
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-26073977-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
 </head>
 
