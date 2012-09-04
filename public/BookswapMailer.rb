c5bae69e17ab20d34b27901e231cc234a30dc8cc
mikehand <mikehand@umich.edu>
2012-09-04 02:12:32 -0400

    get real data and shit

---
+++ public/BookswapMailer.rb
@@ -0,0 +1,10 @@
+class BookswapMailer <ActionMailer::Base
+	default:from=>"TBP Bookswap <tbp-bookswap-web@umich.edu>"
+	@bookswap.location= "EECS Lounge"
+	@bookswap.check_dates="Thursday, Sept. 13 and Friday, Sept. 14 between 10am and 5pm"
+	def send_book_sold_email(person)
+		email_recipient="#{person.name} <#{person.uniqname}@umich.edu>"
+		email_subject ="[TBP Bookswap] Sold Book Notification"
+		mail(:to =>email_recipient, :subject=>email_subject)
+	end
+end
